022bbc991e7a503662b0713000ef22b8afe6b68d
2rajeevmv <2rajeevmv@gmail.com>
2026-02-10 06:18:15 +0530

    setup the feature names & types - save and load to pkl file

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ script to do quick runs and check if everything is working fine - instead of hav
 
 import sys
 import os
+from pprint import pprint
 
 from src.data_utils import (
     load_data,
+    get_dataset_info,
+    get_feature_types,
+    save_feature_names,
+    load_feature_names,
+    get_feature_desc
 )
 
 data_path = './data/german.data'
@@ -16,12 +22,25 @@ def main():
     """
     quick test and check 
     """
-    print("testing and verifying")
+    print(f'\nquick test & verify')
 
     # Load data from local file
-    print("\n[STEP 1] Loading dataset from local file {data_path}...")
+    print(f'\n[1] Loading dataset ...')
     X, y = load_data(filepath= data_path)
+    
+    info = get_dataset_info()
+    pprint(info)
+    
+    column_desc = get_feature_desc()
+    pprint(column_desc, width= 90)
 
+    print(f'\n[2] Feature Types')
+    categorical, numerical = get_feature_types(X)
+    print(f'    categorical ({len(categorical)}): {categorical}')
+    print(f'    numerical ({len(numerical)}): {numerical}')
+
+    print(f'\n[3] Save feature names for later use')
+    #save_feature_names(categorical, numerical)
 
 
 if __name__ == "__main__":
--- src/data_utils.py
+++ src/data_utils.py
@@ -11,6 +11,9 @@ import joblib
 import os
 
 
+data_path = 'data/german.data'
+feature_name_path = 'model/feature_names.pkl'
+
 # Meaningful column names for German Credit Data 
 # This need to be done as:
 # All attribute names have been changed to meaningless symbols to protect confidentiality of the data.
@@ -22,7 +25,20 @@ COLUMN_NAMES = [
     "Job", "NumDependents", "Telephone", "ForeignWorker", "Target"
 ]
 
-data_path = 'data/german.data'
+# Categorical & Numerical Columns
+CATEGORICAL_COLS = [
+    "Status", "CreditHistory", "Purpose", "Savings", "Employment",
+    "PersonalStatusSex", "OtherDebtors", "Property",
+    "OtherInstallmentPlans", "Housing", "Job", "Telephone", "ForeignWorker"
+]
+
+NUMERICAL_COLS = [
+    "Duration", "CreditAmount", "InstallmentRate",
+    "ResidenceDuration", "Age", "ExistingCredits", "NumDependents"
+]
+
+n_features = None
+n_samples = None
 
 ###
 
@@ -37,6 +53,9 @@ def load_data(filepath = data_path):
     """
     
     try:
+        global n_features
+        global n_samples
+
         isloaded = False
         print(f'loading stalog german credit data from {filepath} ...')
         
@@ -51,10 +70,17 @@ def load_data(filepath = data_path):
         X = df.drop('Target', axis=1)
         y = df['Target'].values
 
+        n_samples = X.shape[0]
+        n_features  = X.shape[1]
+        assert len(COLUMN_NAMES) == len(df.columns), "number of columns is not matching!!!"
+
         print(f'dataset loading: {'successful' if  isloaded else 'unsuccessful'}')
-        print(f'  features:{X.shape[1]}')
-        print(f'  instances: {X.shape[0]}')
-        print(f'  columns: {X.columns} ')    
+        print(f'    features:{n_features}')
+        print(f'    instances: {n_samples}')
+        #print(f'    columns: {X.columns} ')    
+        print(f'    number of columns : {len(COLUMN_NAMES)}')
+        #print()
+        print(f'{df.head()}')
 
         return X, y
     
@@ -64,3 +90,83 @@ def load_data(filepath = data_path):
     except Exception as e:
         print(f'error loading data: {e}')
         raise
+
+
+def get_dataset_info():
+    """
+    returns 
+    dict: the dataset information
+    """
+    return {
+        'name': 'Statlog (German Credit Data)',
+        'source': 'UCI Machine Learning Repository',
+        'url': 'https://archive.ics.uci.edu/dataset/144/statlog+german+credit+data',
+        'instances': n_samples,
+        'features': n_features,
+        'task': 'Binary Classification',
+        'target': 'Credit Risk (Good/Bad)',
+        'missing_values': False,
+        'categorical_features': len(CATEGORICAL_COLS),
+        'numerical_features': len(NUMERICAL_COLS)
+    }
+
+def get_feature_types(X):
+    """
+    identify the categorical and numerical features
+    
+    :param X (pd.DataFrame): input features
+    
+    returns
+    categorical (list): categorical column names
+    numerical  (list): numerical column names
+    """
+    categorical = X.select_dtypes(include=['object', 'category']).columns.tolist()
+    numerical = X.select_dtypes(include=[np.number]).columns.tolist()
+
+    return categorical, numerical
+
+def save_feature_names(categorical, numerical, filepath=feature_name_path):
+    
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+
+    feature_names = {
+        'categorical': categorical,
+        'numerical': numerical,
+        'all_features': categorical + numerical
+    }
+
+    joblib.dump(feature_names, filepath, compress=3)
+    print(f'feature name saved to: {filepath}')
+
+def load_feature_names(filepath=feature_name_path):
+    
+    feature_names = joblib.load(filepath)
+
+    return feature_names['categorical'], feature_names['numerical']
+
+
+def get_feature_desc():
+    
+    return {
+        'Status': 'status of existing checking account (categorical)',
+        'Duration': 'duration in months (numerical)',
+        'CreditHistory': 'credit history (categorical)',
+        'Purpose': 'purpose of credit (categorical)',
+        'CreditAmount': 'credit amount (numerical)',
+        'Savings': 'savings account/bonds (categorical)',
+        'Employment': 'present employment since (categorical)',
+        'InstallmentRate': 'installment rate % of disposable income (numerical)',
+        'PersonalStatusSex': 'personal status and sex (categorical)',
+        'OtherDebtors': 'other debtors/guarantors (categorical)',
+        'ResidenceDuration': 'present residence since (numerical)',
+        'Property': 'property (categorical)',
+        'Age': 'age in years (numerical)',
+        'OtherInstallmentPlans': 'other installment plans (categorical)',
+        'Housing': 'housing (categorical)',
+        'ExistingCredits': 'number of existing credits at this bank (numerical)',
+        'Job': 'job (categorical)',
+        'NumDependents': 'number of people being liable to provide maintenance for (numerical)',
+        'Telephone': 'telephone (categorical)',
+        'ForeignWorker': 'foreign worker (categorical)',
+        'Target': 'credit risk: 1=good, 2=bad (converted to 0=good, 1=bad)'
+    }
